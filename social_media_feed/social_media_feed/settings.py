7e94fb2322c43094f87b7d5c5a49b79bccf11df2
kelvyn2012 <kudufo@gmail.com>
2025-11-17 21:02:28 +0100

    feat: integrate and publish GraphQL Playground

--- social_media_feed/social_media_feed/settings.py
+++ social_media_feed/social_media_feed/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "social_feed.onrender.com"]
+
 
 
 # Application definition
